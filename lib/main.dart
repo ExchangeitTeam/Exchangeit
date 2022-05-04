bb29784af55667e955fee2e7378468175ee1d408
QuZu <emertkuzu35@gmail.com>
2022-05-04 19:36:57 +0300

    update main page

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:exchangeit/routes/welcomepage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    // initialRoute: '/login',
+    routes: {
+      '/': (context) => const WelcomePage(),
+      //SignUp.routeName: (context) => SignUp(),
+      //Login.routeName: (context) => Login(),
+    },
+  ));
+}
