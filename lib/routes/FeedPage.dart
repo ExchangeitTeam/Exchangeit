1a6b43b9aa369e15f883235d3f73cb6e14a63bfb
QuZu <emertkuzu35@gmail.com>
2022-05-18 23:42:01 +0300

    update Feed page

--- lib/routes/FeedPage.dart
+++ lib/routes/FeedPage.dart
@@ -10,8 +10,26 @@ class FeedPage extends StatefulWidget {
 class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Text("FeedPage")],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exchangeit'),
+        elevation: 0,
+        foregroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 0, 170, 229),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [Text("FeedPage")],
+      ),
     );
   }
 }
