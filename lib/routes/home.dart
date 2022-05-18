78025fa255c18b97ac92fd13529e0ef2d9d1fbd7
hiDeniz <halilibrahimdeniz2001@gmail.com>
2022-05-18 18:30:58 +0300

    main_page_add

---
+++ lib/routes/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  PageController pageController = PageController();
+
+  void onTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+    pageController.animateToPage(index, duration: Duration(milliseconds: 1000), curve: Curves.ease);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exchangeit'),
+        elevation: 0,
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue.shade600,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+
+      body: PageView(
+        controller: pageController,
+        children: [
+          Container(color: Colors.red),
+          Container(color: Colors.orange),
+          Container(color: Colors.yellow),
+          Container(color: Colors.green),
+          Container(color: Colors.purple),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.add_circle_outline), label: 'Add'),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifications'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Account'),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue.shade600,
+          unselectedItemColor: Colors.black,
+          onTap: onTapped,
+      ),
+    );
+  }
+}
