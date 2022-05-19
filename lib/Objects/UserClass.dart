7d6ad064545f4324bd1976270c8a531bda146fd4
QuZu <emertkuzu35@gmail.com>
2022-05-20 02:22:17 +0300

    fix class

--- lib/Objects/UserClass.dart
+++ lib/Objects/UserClass.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'PostClass.dart';
+
 class SettingUser {
   String university = '';
   String username = '';
@@ -8,4 +10,31 @@ class SettingUser {
   String profile_image = '';
   SettingUser(
       this.username, this.university, this.age, this.bio, this.profile_image);
+}
+
+class User {
+  String id = "";
+  late NetworkImage profileImage;
+  String university = '';
+  String username = '';
+  String age = '';
+  String bio = '';
+  String profile_image = '';
+  List<User> followers = [];
+  List<User> following = [];
+  List<String> comments = [];
+  List<PostBase> Posts = [];
+
+  User(
+    this.id,
+    this.profileImage,
+    this.username,
+    this.university,
+    this.age,
+    this.bio,
+    this.profile_image,
+    this.followers,
+    this.following,
+    this.Posts,
+  );
 }
