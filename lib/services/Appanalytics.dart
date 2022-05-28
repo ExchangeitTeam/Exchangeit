2edc20eead346d422183a309cd6830866f42284e
QuZu <emertkuzu35@gmail.com>
2022-05-28 13:16:41 +0300

    fix func names

--- lib/services/Appanalytics.dart
+++ lib/services/Appanalytics.dart
@@ -2,7 +2,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 
 class AppAnalytics {
   static final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-  static Future<void> setLogEventUtil(
+  static Future<void> setLogEvent(
       {required String eventName, Map<String, dynamic>? params}) async {
     await analytics.logEvent(
       name: eventName,
@@ -10,11 +10,11 @@ class AppAnalytics {
     );
   }
 
-  static Future<void> setCurrentScreenUtil({required String screenName}) async {
+  static Future<void> setCurrentScreen({required String screenName}) async {
     await analytics.setCurrentScreen(screenName: screenName);
   }
 
-  static Future<void> setUserIdUtil({required String userId}) async {
+  static Future<void> setUserId({required String userId}) async {
     await analytics.setUserId(id: userId);
   }
 }
